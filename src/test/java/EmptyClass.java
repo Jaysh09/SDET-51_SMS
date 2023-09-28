84771cb4afcd540c7d16f0c0ca24676c37e9424b
jayas <jayas@DESKTOP-L0TMCC0>
2023-09-28 10:11:34 +0530

    commit3

---
+++ src/test/java/EmptyClass.java
@@ -0,0 +1,11 @@
+
+public class EmptyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is an empty class");
+		System.out.println("Make changes as per your wish");
+			System.out.println("I dont want class on Saturday");
+
+	}
+
+}
